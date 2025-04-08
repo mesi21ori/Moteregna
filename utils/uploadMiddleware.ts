d81e42631c0e7423c4cd13fc19fb5b4b22b20959
mesi21ori <mesiori21@gmail.com>
2025-04-08 18:05:41 +0300

    update last confilct

--- utils/uploadMiddleware.ts
+++ utils/uploadMiddleware.ts
@@ -1,35 +1,13 @@
-// import multer from 'multer';
-// import path from 'path';
-
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, './public/uploads/'); 
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + path.extname(file.originalname)); 
-//   }
-// });
-
-// const upload = multer({ storage: storage }).fields([
-//   { name: 'Librephoto', maxCount: 1 },
-//   { name: 'driversLicencephotoFront', maxCount: 1 },
-//   { name: 'profile', maxCount: 1 },
-//   { name: 'businessPermit', maxCount: 1 }
-// ]);
-
-// export default upload;
-
 import multer from 'multer';
 import path from 'path';
-import { NextApiRequest, NextApiResponse } from 'next';
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/uploads/');
+    cb(null, './public/uploads/'); 
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, Date.now() + path.extname(file.originalname)); 
   }
 });
 
@@ -42,11 +20,3 @@ const upload = multer({ storage: storage }).fields([
 
 export default upload;
 
-// Add type extensions for NextApiRequest
-declare module 'next' {
-  interface NextApiRequest {
-    files?: {
-      [fieldname: string]: Express.Multer.File[];
-    };
-  }
-}
