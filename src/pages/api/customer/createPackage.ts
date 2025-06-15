d30e6fa76bce471c2a0a0d811b42779eccffa1da
JonathanZeru <jonathanzeru21@gmail.com>
2025-06-15 22:43:52 +0300

    jhbnm

--- src/pages/api/customer/createPackage.ts
+++ src/pages/api/customer/createPackage.ts
@@ -6,22 +6,7 @@ import axios from 'axios';
 // import admin from '@/firebase/firebaseAdmin';
 import * as admin from "firebase-admin";
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: "moteregna-a44d2",
-      // privateKeyId: "2d8bef425c9ab8e9c780f13f25c58e12a8e9f6a0",
-      privateKey: process.env.PRIVATE_KEY,
-      clientEmail: "firebase-adminsdk-fbsvc@moteregna-a44d2.iam.gserviceaccount.com",
-      // clientId: "100040438321198845883",
-      // authUri: "https://accounts.google.com/o/oauth2/auth",
-      // tokenUri: "https://oauth2.googleapis.com/token",
-      // authProviderX509CertUrl: "https://www.googleapis.com/oauth2/v1/certs",
-      // clientC509CertUrl: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40moteregna-a44d2.iam.gserviceaccount.com",
-      // universeDomain: "googleapis.com"
-    }),
-  });
-}
+
 const createPackageSchema = z.object({
   description: z.string().min(1, "Description is required"),
   categoryId: z.string().min(1, "Category ID is required"), 
